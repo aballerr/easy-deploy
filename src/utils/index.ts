60326a68acbb3a49bcd1301f7f735a9abe73d3dc
aballerr <aball9423@gmail.com>
2023-04-08 11:18:57 -0400

    slight change to get correct directory

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import finder from "find-package-json";
 
 export const findRootDir = (): String => {
-  const result = finder(__dirname);
+  const result = finder(process.cwd());
   const baseDirectory = result.next().value?.__path || "";
   const rootPath = baseDirectory.replace("/package.json", "");
 
